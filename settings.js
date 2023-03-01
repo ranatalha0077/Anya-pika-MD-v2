a2554f69a080d3f4c36bdc0787bd95d0385eb680
ranatalha0077 <126395038+ranatalha0077@users.noreply.github.com>
2023-03-01 10:19:48 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -12,16 +12,16 @@ global.headerText = "Anya by Pika" // text for header of menu
 global.DocumentTitle = "MASTER⚔️PIKA" 
 
 
-global.ownername = "𝕻𝖎𝖐𝖆♰𝕶𝖚𝖓"   // Owner name //        
-global.ownernumber = ['916900904828'] 
-global.socialm = "𝗜𝗻𝘀𝘁𝗮 : 3.69_pika" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
+global.ownername = "RANA TALHA"   // Owner name //        
+global.ownernumber = ['923010277751'] 
+global.socialm = "𝗜𝗻𝘀𝘁𝗮 : itzs._.rana" // 𝗜𝗻𝘀𝘁𝗮 : || 𝗚𝗶𝘁𝗵𝘂𝗯 : || 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 : \\
 global.ytname = "YT: Ytname"   // Yt chanel name //
 global.continent = "Asia" // your continent name 
-global.region = "India" // your country name
-global.state = "Assam" // your state name
-global.district = "Dibrugarh" // your district name
-global.timezone = "Asia/Kolkata" // search on google if you don't know the timezone of your country //
-global.instagramId = "3.69_pika" // Your insta ID
+global.region = "Pakistan" // your country name
+global.state = "Punjab" // your state name
+global.district = "Lahore" // your district name
+global.timezone = "Asia/Islamabad" // search on google if you don't know the timezone of your country //
+global.instagramId = "itzs._.rana" // Your insta ID
 global.email = "example@exmaple.com" // put your email here
 
 
